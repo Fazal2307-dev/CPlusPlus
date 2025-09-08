8c368d8904e41e5299cdc57b67c34b890dceab47
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-08 17:06:46 +0530

    volume of sphere

---
+++ volumeofsphere.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+#define pi=3.14;
+int main(){
+    int n;
+    cout<<"Enter value of n:\n";
+    cin>>n;
+    int sphere=(4*3.14*n*n*n)/3;
+    cout<<sphere;
+    return 0;
+
+}

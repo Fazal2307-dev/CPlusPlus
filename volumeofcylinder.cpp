c33ef69d7f1ed094645397fd35f1d85d48023b33
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-08 17:15:36 +0530

    volume of cylinder

---
+++ volumeofcylinder.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int height;
+    cout<<"Enter value of n,and height:\n";
+    cin>>n>>height;
+    i cylinder = (3.14*n*n*height);
+    cout<<cylinder;
+    return 0;
+}

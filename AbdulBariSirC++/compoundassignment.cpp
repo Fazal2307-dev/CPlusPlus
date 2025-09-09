02db2838c87acd9801b73f0f6f42267a5b518d4a
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-09 19:43:39 +0530

    compound assingment

---
+++ AbdulBariSirC++/compoundassignment.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter value on a:\n";
+    cin>>a;
+    int sum=0;
+    sum+=a;
+    cout<<"Value of a:"<<a<<endl;
+    return 0;
+}

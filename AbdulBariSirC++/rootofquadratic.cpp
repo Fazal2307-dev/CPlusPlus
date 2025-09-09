1d42658edc9a23b3a4c471c3c72fa0cf7f3dcf1a
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-09 19:28:40 +0530

    root of quadratic

--- AbdulBariSirC++/rootofquadratic.cpp
+++ AbdulBariSirC++/rootofquadratic.cpp
@@ -2,10 +2,21 @@
 #include<cmath>
 using namespace std;
 int main(){
-    int b,a,c;
-    cout<<"Enter value of a,b and c:\n ";
+    float a,b,c;
+    cout<<"Enter value of a,b and c:\n";
     cin>>a>>b>>c;
-    int root= (-b+sqrt((b*b)-(4*a*c)))/(2*a);
-    cout<<"Root of quadratic equation"<<root;
+    float D=(b*b-4*a*c);
+    if(D>0){
+        float root1=(-b+sqrt(D))/(2*a);
+        float root2=(-b+sqrt(D))/(2*a);
+        cout<<"Two real and distinct value:"<< root1 <<"and"<<root2<<endl;
+    }
+    else if(D==0){
+        float root3 = -b/(2+a);
+        cout<<"One real value:"<<root3<<endl;
+    }
+    else{
+        cout<<"No is imaginary"<<endl;
+    }
     return 0;
 }

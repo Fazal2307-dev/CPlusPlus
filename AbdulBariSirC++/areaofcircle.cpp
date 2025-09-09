6e2e3d8a1ff6d025bcdaffdccda705b9f0c2fe03
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-09 19:32:44 +0530

    area of circle

---
+++ AbdulBariSirC++/areaofcircle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r;
+    cout<<"Enter radius of circle:\n";
+    cin>>r;
+    float AreaOfCircle = 3.14*r*r;
+    cout<<"Area of circle is:"<<AreaOfCircle<<endl;
+    return  0;
+}

33bbadf84179417d6f1048afbc396c24f654f602
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-09 20:21:43 +0530

    area of triangle

---
+++ AbdulBariSirC++/areaoftriangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int base;
+    int height;
+    cout<<"Enter vaalue of base:";
+    cin>>base;
+    cout<<"enter value of height:";
+    cin>>height;
+    float areaOfTriangle= (base*height)/2;
+    cout<<"AreaOfTriangle:"<< areaOfTriangle<<endl;
+    return 0;
+}

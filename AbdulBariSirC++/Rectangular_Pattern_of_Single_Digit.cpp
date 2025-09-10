df5dc0d17ea50119f535e0932337dda4f3775001
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-10 12:31:58 +0530

    Rectangular Pattern of Single Digit

---
+++ AbdulBariSirC++/Rectangular_Pattern_of_Single_Digit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter value of n:\n";
+    cin>>n;
+    for(int i=1;i<=6;i++){
+        for(int j=1;j<=4;j++){
+            cout<< n ;
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}

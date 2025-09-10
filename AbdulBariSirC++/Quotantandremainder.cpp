528809df6012c91dac29e4541a37b8c2f823e5e0
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-10 12:21:07 +0530

    quatent and remainder

---
+++ AbdulBariSirC++/Quotantandremainder.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int divident;
+cout<<"Enter value of divident:";
+cin>>divident;
+int divisor;
+cout<<"Enter value of divisor:";
+cin>>divisor;
+int quatent = divident/divisor;
+int remainder = divident % divisor;
+cout<<quatent<<endl;
+cout<<remainder<<endl;
+return 0;
+
+}

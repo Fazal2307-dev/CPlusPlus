366f94c2802b345556375556a4bb8b16d2220148
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-10 17:22:25 +0530

    Maximum of 2 Numbers

---
+++ AbdulBariSirC++/10/09/2025/conditional/Maximum_of_2_Numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter value of a and b:";
+    cin>>a>>b;
+    if(a>b)
+    cout<<"A is bigger than B"<<endl;
+    else
+    cout<<"B is bigger than A"<<endl;
+    return 0;
+    
+}

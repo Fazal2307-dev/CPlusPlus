40862b537e808d2f9e331670aa92f641ed4f432f
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-10 19:54:26 +0530

    do while

---
+++ AbdulBariSirC++/10/09/2025/conditional/do_while.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i=1;
+    cout<<"enter vaalue of n:";
+    cin>>n;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i>n);
+    return 0;
+}

8a12687f17841c75b73fc2f7168a3d6975b766da
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-11 18:37:48 +0530

    array

--- AbdulBariSirC++/september_11/01Array.cpp
+++ AbdulBariSirC++/september_11/01Array.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a[5]={};
-    for(int i=0;i<5;i++){
+    int n;
+    cout<<"Enter how many array you want:";
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
         cout<<a[i]<<endl;
     }
     return 0;

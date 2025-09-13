d362511d3ea6f5028eef8059ea7ee0d298422d67
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-13 12:56:29 +0530

    largest element in array

--- w3resource/c++/ArrayQuestion/Largest_Element_in_Array.cpp
+++ w3resource/c++/ArrayQuestion/Largest_Element_in_Array.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 int main(){
     int a[10]={12,45,78,98,23,46,21,26,28,29,};
-    for(int i=0;i<10;i++){
-        cout<<a[i]<<" ";
+    int max;
+    max=a[0];
+    for(int i=1;i<10;i++){
+        if(max<a[i]){
+            max=a[i];
+        }
     }
+       cout<<max<<endl;
     return 0;
 }

c98c6f0fc824281e9062de4d9f1f0e969a1b0c6e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-15 12:13:33 +0530

    u can use cin.ignore for taking two input

--- AbdulBariSirC++/september_15/02_reading_and_printing.cpp
+++ AbdulBariSirC++/september_15/02_reading_and_printing.cpp
@@ -3,9 +3,17 @@ using namespace std;
 int main(){
     char s[20];
     cout<<"Enter you s[] value:";
-    // cin>>s
-    // cin.get(s,20);
-    cin.getline(s,20);
+    cin>>s;
+    cin.get(s,20);
     cout<<s<<endl;
+    char s2[20];
+    cout<<"Enter your name again:";
+    cin.get(s2,20);
+    cout<<s2<<endl;
+    // cin.getline(s,20);
+    // cout<<s<<endl;
+
     return 0;
 }
+//if you taking two input with the help cin.get() you will not get second value 
+//if you want 2nd value as input you can do cin.get( first value ) cin.ignore() cin.get(second value )

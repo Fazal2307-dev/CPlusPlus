078f3e00d1651c032ba51968b7e75c8e2919aadb
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-15 12:31:42 +0530

    string concatination

--- AbdulBariSirC++/september_15/03_function_in_string_h_cstring.cpp
+++ AbdulBariSirC++/september_15/03_function_in_string_h_cstring.cpp
@@ -2,10 +2,21 @@
 #include<cstring>
 using namespace std;
 int main(){
-    char s[10];
-cout<<"Enter your string:";
-cin.getline(s,100);
-cout<<"Lenght "<<strlen(s)<<endl;
+//     char s[10];
+// cout<<"Enter your string:";
+// cin.getline(s,100);
+// cout<<"Lenght "<<strlen(s)<<endl;
+char s1[20]="Good";
+char s2[20]="Morining";
+strcat(s1,s2);
+cout<<s1<<endl;
+cout<<s2<<endl;
+char s[10]="Fazal";
+char t[10]="Anjum";
+strncat(s,t,4);
+cout<<s<<endl;
+
+
 return 0;
 
 }

e4a0ad9b939d77e068d72cce8723a8de61066985
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-15 12:04:35 +0530

    using cin.get(),cin.getline()

--- AbdulBariSirC++/september_15/02_reading_and_printing.cpp
+++ AbdulBariSirC++/september_15/02_reading_and_printing.cpp
@@ -3,7 +3,9 @@ using namespace std;
 int main(){
     char s[20];
     cout<<"Enter you s[] value:";
-    cin>>s;
+    // cin>>s
+    // cin.get(s,20);
+    cin.getline(s,20);
     cout<<s<<endl;
     return 0;
 }

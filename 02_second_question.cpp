42677ead379bb52ed7e55dc0d53de2e64f12e852
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-15 09:50:00 +0530

    002 second question

---
+++ 02_second_question.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter value of n:";
+    cin>>n;
+    if(n%2==0)
+    cout<<"N is even no:";
+    else
+    cout<<"N is odd:";
+    return 0;
+}

5dfc5b656dfa1a8816ad215aee282b521708bd7f
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-15 13:45:06 +0530

    strcmp

--- AbdulBariSirC++/september_15/04_string_substring.cpp
+++ AbdulBariSirC++/september_15/04_string_substring.cpp
@@ -2,14 +2,21 @@
 #include<cstring>
 using namespace std;
 int main(){
-    char s1[20]="programming";
-    char s2[20]="gram";
-    //strstr(main,sub);
+    // char s1[20]="programming";
+    // char s2[20]="gram";
+    //1.strstr(main,sub);
     // cout<<strstr(s1,s2)<<endl;
-    //strchr(main,char)
+    //2.strchr(main,char)
     // cout<<strchr(s1,'i')<<endl;
-    //strrchr()
-    cout<<strrchr(s1,'n')<<endl;
+    //3.strrchr()
+    //cout<<strrchr(s1,'n')<<endl;
+    //4.strcmp(str1,str2)
+    // char s1[10]="hello";
+    // char s2[10]="fazal";
+    // cout<<strcmp(s1,s2)<<endl;
+ char s1[10]="heLlo";
+ char s2[10]="hello";
+ cout<<strcmp(s1,s2)<<endl;
 
 
 

e1405e3ff49a92b6006844c7cfef870ae646081d
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-15 12:22:24 +0530

    using string.h

---
+++ AbdulBariSirC++/september_15/03_function_in_string_h_cstring.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+    char s[10];
+cout<<"Enter your string:";
+cin.getline(s,100);
+cout<<"Lenght "<<strlen(s)<<endl;
+return 0;
+
+}

4dad20e28980d1d69f7a892ee469f5e892fd6b4c
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-15 13:36:49 +0530

    strstr() function string

---
+++ AbdulBariSirC++/september_15/04_string_substring.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+    char s1[20]="programming";
+    char s2[20]="gram";
+    //strstr(main,sub);
+    // cout<<strstr(s1,s2)<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}

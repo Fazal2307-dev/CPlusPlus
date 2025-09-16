ee64fac29a2f4df489f07ec8e3a867aa6c7e970e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-16 23:16:07 +0530

    copy(),find(),rfind()

---
+++ AbdulBariSirC++/september_16/02_class_string.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    //s.copy()
+// string s="welcome";
+// char str[10];
+// s.copy(str,s.length());
+// cout<<s<<endl;
+
+//s.find()
+string str ="how are you";
+str.find("are");
+str.rfind('o');
+cout<<str<<endl;
+cout<<str<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}

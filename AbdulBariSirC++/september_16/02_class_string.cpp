d0df4a80773fe717e0f9d507c32598e4a3fb6634
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-16 23:32:32 +0530

    concatination

--- AbdulBariSirC++/september_16/02_class_string.cpp
+++ AbdulBariSirC++/september_16/02_class_string.cpp
@@ -16,11 +16,19 @@ int main(){
 // cout<<str<<endl
 
 // s.substr()
-string str="programming";
-string f =str.substr(3);
-cout<<f<<endl;
+// string str="programming";
+// string f =str.substr(3);
+// cout<<f<<endl;
 
+// //at();
+// string str="Holidaay";
+// string str1 = str.at(4);
+// cout<<str1<<endl;
 
+string s1="hello";
+string s2= "World";
+string s3= s1+s2;
+cout<<s3<<endl;
 
 
 

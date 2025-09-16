ffdbe2495e595a9931f008b1c836d6ea1abbfed0
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-16 23:35:33 +0530

    using for loop in string

---
+++ AbdulBariSirC++/september_16/03_using_for_loop_in_string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str="Welcome";
+    for(int i=0;str[i]!='\0';i++){
+        cout<<str[i];
+           cout<<endl;
+    }
+ 
+    return 0;
+}

3be4ce29f778b54514b111cb9dea66e3baeeb799
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-17 00:15:39 +0530

    mix using value

---
+++ AbdulBariSirC++/september_16/06_mix_upper_lower.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str="WeLCoME";
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]>=97 && str[i]<=122){
+            str[i]=str[i]-32;
+        }
+    }
+    cout<<str<<endl;
+    return 0;
+}

79fedb047138858264ec6ac0c2d9c2ebe8ea5660
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-16 19:07:53 +0530

    s.append()

---
+++ AbdulBariSirC++/september_16/01_class_string.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str="Hello";
+    //str.append()
+    // str.append(" World");
+    string str2="world";
+    str.append(str2);
+    cout<<str<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}

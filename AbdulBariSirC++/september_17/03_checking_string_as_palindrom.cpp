e8423416ee3e9da511fe2b171fb92224c02f3f99
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-17 12:01:21 +0530

    03 checking palindrom

---
+++ AbdulBariSirC++/september_17/03_checking_string_as_palindrom.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str="MADAM";
+    string rev=" ";
+    int len = str.length();
+    rev.resize(len);
+    for(int i=0, j=len-1;i < len;i++,j--){
+        rev[i]=str[j];
+    }
+    rev[len]='\0';
+    if(str.compare(rev)==0)
+    cout<<"Strin is Palindrom"<<endl;
+    else
+    cout<<"String is not palindrom"<<endl;
+    return 0;
+}

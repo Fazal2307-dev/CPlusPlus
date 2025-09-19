260ca3fe8e0dd377241beeb7aa44db5d84259ebe
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-19 11:49:51 +0530

    02_q

---
+++ AbdulBariSirC++/september_19/02_q.cpp
@@ -0,0 +1,31 @@
+// Lucky Seven
+// Chef considers the number 
+// 7
+// 7 lucky. As a result, he believes that the 
+// 7
+// 7-th letter he sees on a day is his lucky letter of the day.
+
+// You are given a string 
+// S
+// S of length 
+// 10
+// 10, denoting the first 
+// 10
+// 10 letters Chef saw today.
+// What is Chef's lucky letter?
+
+// Input Format
+// The only line of input contains a string 
+// S
+// S, of length 
+// 10
+// 10.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+string s;
+getline(cin,s);
+cout<<s[6]<<endl;
+}

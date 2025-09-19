2e31b64a996d80d05aaca71bbc946d7dfb3f9af6
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-19 11:54:42 +0530

    04_q

---
+++ AbdulBariSirC++/september_19/04_q.cpp
@@ -0,0 +1,25 @@
+// Double Rent
+// Chefina decided to move into Chef's apartment.
+// Chef was initially paying a rent of 
+// X
+// X rupees. Since Chefina is moving in, the owner decided to double the rent.
+
+// Find the final rent Chef needs to pay.
+
+// Input Format
+// The input consists of a single integer 
+// X
+// X, denoting the rent Chef was initially paying.
+
+// Output Format
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	cout<<2*x<<endl;
+	return 0;
+
+}

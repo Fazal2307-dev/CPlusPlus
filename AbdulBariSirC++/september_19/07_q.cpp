cc98242ad044cd36ec10b0f25a9ef5ec64eeb926
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-19 12:00:15 +0530

    07_q

---
+++ AbdulBariSirC++/september_19/07_q.cpp
@@ -0,0 +1,36 @@
+// Biryani classes
+// According to a recent survey, Biryani is the most ordered food. Chef wants to learn how to make world-class Biryani from a MasterChef. Chef will be required to attend the MasterChef's classes for 
+// X
+// X weeks, and the cost of classes per week is 
+// Y
+// Y coins. What is the total amount of money that Chef will have to pay?
+
+// Input Format
+// The first line of input will contain an integer 
+// T
+// T — the number of test cases. The description of 
+// T
+// T test cases follows.
+// The first and only line of each test case contains two space-separated integers 
+// X
+// X and 
+// Y
+// Y, as described in the problem statement.
+// Output Format
+// For each test case, output on a new line the total amount of money that Chef will have to pay.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int x,y;
+    cin>>x>>y;
+    cout<<x*y<<endl;
+}
+return 0;
+}
+
+

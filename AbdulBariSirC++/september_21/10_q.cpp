d4db6ee91a5cb55b86fb3ca635e450bc73cdc72f
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-21 18:23:18 +0530

    chess

---
+++ AbdulBariSirC++/september_21/10_q.cpp
@@ -0,0 +1,38 @@
+// Chess Time
+// Chef has recently started playing chess, and wants to play as many games as possible.
+
+// He calculated that playing one game of chess takes at least 
+// 20
+// 20 minutes of his time.
+
+// Chef has 
+// N
+// N hours of free time. What is the maximum number of complete chess games he can play in that time?
+
+// Input Format
+// The first line of input will contain a single integer 
+// T
+// T, denoting the number of test cases.
+// Each test case consists of a single line containing a single integer, 
+// N
+// N.
+// Output Format
+// For each test case, output on a new line the maximum number of complete chess games Chef can play in 
+// N
+// N hours.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    cout<<n*3;
+	    cout<<endl;
+	    
+	}
+	return 0;
+
+}

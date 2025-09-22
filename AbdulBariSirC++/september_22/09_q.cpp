810b7e760bdb3b78d3061e3757ae541ace36a061
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-22 19:10:29 +0530

    how much hour it take to cool 2 litre

---
+++ AbdulBariSirC++/september_22/09_q.cpp
@@ -0,0 +1,36 @@
+// Water Requirement
+// Finally, after purchasing a water cooler during the April long challenge, Chef noticed that his water cooler requires 
+// 2
+// 2 liters of water to cool for one hour.
+
+// How much water (in liters) would be required by the cooler to cool for 
+// N
+// N hours?
+
+// Input Format
+// The first line of input will contain an integer 
+// T
+// T — the number of test cases. The description of 
+// T
+// T test cases follows.
+// The first and only line of each test case contains an integer 
+// N
+// N, as described in the problem statement.
+// Output Format
+// For each test case, output the number of liters of water required by the water cooler to cool for 
+// N
+// N hours.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    cout<<2*n<<endl;
+	}
+	return 0;
+
+}

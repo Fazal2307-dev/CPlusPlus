12947104c5c9d0b15bfd53fcd19d64f8dd767aa4
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-22 23:26:55 +0530

    checking how much coin i  have to gave

---
+++ AbdulBariSirC++/september_22/15_q.cpp
@@ -0,0 +1,42 @@
+// Minimum Coins
+// There are only 
+// 2
+// 2 type of denominations in Chefland:
+
+// Coins worth 
+// 1
+// 1 rupee each
+// Notes worth 
+// 10
+// 10 rupees each
+// Chef wants to pay his friend exactly 
+// X
+// X rupees. What is the minimum number of coins Chef needs to pay exactly 
+// X
+// X rupees?
+
+// Input Format
+// The first line of input will contain a single integer 
+// T
+// T, denoting the number of test cases.
+// Each test case consists of a single line of input containing a single integer 
+// X
+// X.
+// Output Format
+// For each test case, output on a new line the minimum number of coins Chef needs to pay exactly 
+// X
+// X rupees.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while(t--){
+    int x;
+    cin>>x;
+    cout<<x%10<<endl;
+}
+return 0;
+
+}
